e9f4507beaa79dabb77e3a27712dd6daa793946d
Jainish Patel <jainish@goalkeep.net>
2023-02-14 13:12:24 +0000

    formatted fct_quantitative_summary

--- models/marts/fct_quantitative_summary.sql
+++ models/marts/fct_quantitative_summary.sql
@@ -2,31 +2,37 @@
 
 with
     source1 as (select * from {{ ref("int_joined_tables") }}),
-    source2 as (select * from {{ ref("int_aggregate_by_review_type") }})
+    source2 as (select * from {{ ref("int_aggregate_by_review_type") }}),
 
-    SELECT 
-        filled_for,
-        year_and_quarter,
-        question_number,
-        self_rating,
-        peer_rating,
-        managee_rating,
-        manager_rating,
-        short_form,
-        long_form,
-        overall_avg_rating
-   FROM source1
+    union_tables as (
+        select
+            filled_for,
+            year_and_quarter,
+            question_number,
+            self_rating,
+            peer_rating,
+            managee_rating,
+            manager_rating,
+            short_form,
+            long_form,
+            overall_avg_rating
+        from source1
 
-UNION ALL
- SELECT 
-    filled_for,
-    year_and_quarter,
-    question_number,
-    self_rating,
-    peer_rating,
-    managee_rating,
-    manager_rating,
-    short_form,
-    long_form,
-    overall_rating_by_review_type AS overall_avg_rating
-   from source2
+        union all
+
+        select
+            filled_for,
+            year_and_quarter,
+            question_number,
+            self_rating,
+            peer_rating,
+            managee_rating,
+            manager_rating,
+            short_form,
+            long_form,
+            overall_rating_by_review_type as overall_avg_rating
+        from source2
+    )
+
+select *
+from union_tables
